f7e35098a6045b493e92d253613a3502d04a628d
Santiago Botero <santibotero2012@gmail.com>
2023-02-09 10:22:18 -0500

    microfono arreglado

--- assets/js/pop-up-recepcion.js
+++ assets/js/pop-up-recepcion.js
@@ -24,4 +24,56 @@ document.addEventListener('DOMContentLoaded', function() {
     centro_informacion_popup_acceso.setAttribute('scale', '0 0 0');
     });
     
+
+    const closePopup_Reportes1 = document.querySelector('#cerrar_consulta1');
+    const popup_Reportes1 = document.querySelector('#logo_consulta1');
+    const information_Reportes1 = document.querySelector('#popup_consulta1');
+
+    if (popup_Reportes1) popup_Reportes1.addEventListener('click', function (e) {
+        information_Reportes1.setAttribute('scale', '1 1 1');
     });
+    if (closePopup_Reportes1) closePopup_Reportes1.addEventListener('click', function (e) {
+        information_Reportes1.setAttribute('scale', '0 0 0');
+    });
+    
+    //Grabación de voz 1
+    const grabar = document.querySelector('#microfono1');
+    const texto = document.querySelector('#Text1');
+
+    let recognition=new webkitSpeechRecognition();
+    recognition.lang='es-mx';
+    recognition.continuous = false;
+    recognition.interimResults=false; //No devuelve resultados parciales
+
+    recognition.onresult = (event) => {
+        const results=event.results; 
+        const frase= results[results.length-1][0].transcript;
+        //console.log(frase);
+        texto.setAttribute('text', 'value:'+frase);
+        //texto.value =frase;
+    }
+
+    if (grabar) grabar.addEventListener('click', function (e) {
+        console.log('Start recording');
+        recognition.start();
+        
+    })
+    //Fin grabación
+    const consultar = document.querySelector('#boton_consultar');
+    if (consultar) consultar.addEventListener('click', function (e) {
+        const consulta=texto.getAttribute('text');
+        value=Object.values(consulta)[0]
+        palabra='';
+        for (i in value){
+            if(value[i]!==' '){
+                //console.log(value[i]);
+                palabra+=value[i]
+            }
+        }
+        console.log(value);
+        consulta_formato='SR'+value;
+        console.log(consulta_formato);
+        url='https://fa-eset-dev4-saasfademo1.ds-fa.oraclepdemos.com/crmRestApi/resources/11.13.18.05/serviceRequests/'+consulta_formato;
+        console.log('URL: ', url);
+    });
+});
